37084ba877d286c56c23aec0fc7919dd1ac0a86f
Miko Dagatan <miguel.dagatan@gmail.com>
2024-01-17 07:49:56 +0800

    Update mail and s3 credentials

--- config/initializers/s3_upload.rb
+++ config/initializers/s3_upload.rb
@@ -1,8 +1,11 @@
 require 'aws-sdk-s3'
 
 Aws.config.update(
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  region: ENV['BUCKETEER_AWS_REGION'] || ENV['AWS_REGION'],
+  credentials: Aws::Credentials.new(
+    ENV['BUCKETEER_AWS_ACCESS_KEY_ID'] || ENV['AWS_ACCESS_KEY_ID'],
+    ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY'] || ENV['AWS_SECRET_ACCESS_KEY']
+  )
 )
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['AWS_S3_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKETEER_BUCKET_NAME'] || ENV['AWS_S3_BUCKET'])
