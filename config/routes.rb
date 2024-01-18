32dea6310c2e814d5bd6133189a1f0469bd1ed5e
Miko Dagatan <miguel.dagatan@gmail.com>
2024-01-18 20:39:01 +0800

    Added sidekiq web (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
   mount ActionCable.server => '/cable'
 
+  mount Sidekiq::Web => '/sidekiq'
+
   # Defines the root path route ("/")
   # root "posts#index"
 
